1b6a2e9f26bfcad14fa365e629df364186405eb1
jazi200 <95362234+jazi200@users.noreply.github.com>
2022-05-03 23:11:09 +0600

    Add files via upload

---
+++ lab1_project/project/tests/test_models.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from ..models import *
+class YourTestClass(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        print("setUpTestData: Run once to set up non-modified data for all class methods.")
+        pass
+    def setUp(self):
+        print("setUp: Run once for every test method to setup clean data.")
+        pass
+    def test_false_is_false(self):
+        print("Method: test_false_is_false.")
+        self.assertFalse(False)
+
+    def test_false_is_true(self):
+        print("Method: test_false_is_true.")
+        post = Pikir()
+        self.assertTrue(post.get_number())
+    
+    
+    def test_one_plus_one_equals_two(self):
+        print("Method: test_one_plus_one_equals_two.")
+        self.assertEqual(1+1,2)
+    
+    '''
+    def test_compare_number(self):
+        post = Pikir()
+        self.assertEqual(post.get_number(),9)
+    '''
